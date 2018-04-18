82ea5921cef99c1ec197a9c3174ed8793c83fa2f
Eddie Webb <ollitech@gmail.com>
2018-04-18 11:12:47 -0400

    expose env details to illustrate blue/green

--- src/main/java/com/edwardawebb/circleci/demo/controllers/HomeController.java
+++ src/main/java/com/edwardawebb/circleci/demo/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     public static final String ABOUT_VIEW = "about";
-    public static final String CONTACT_VIEW = "contact";
+
 
     @RequestMapping("/about")
     public String about(Model model) {
