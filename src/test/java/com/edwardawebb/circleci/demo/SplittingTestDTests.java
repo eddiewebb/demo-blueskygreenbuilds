7c5fcab4fd356ab4e8cab62f968d80ddc0145158
Eddie Webb <ollitech@gmail.com>
2020-01-10 16:05:04 -0500

    more tests

---
+++ src/test/java/com/edwardawebb/circleci/demo/SplittingTestDTests.java
@@ -0,0 +1,50 @@
+package com.edwardawebb.circleci.demo;
+
+import org.junit.Test;
+
+public class SplittingTestDTests {
+
+
+    @Test
+    public void testThatTakesOneTenthSeconds() throws Exception {
+        Thread.sleep(100);
+    }
+
+    @Test
+    public void testThatTakesTwoTenthSeconds() throws Exception {
+        Thread.sleep(200);
+    }
+
+    @Test
+    public void testThatTakesThreeTenthSeconds() throws Exception {
+        Thread.sleep(300);
+    }
+
+    @Test
+    public void testThatTakesFourTenthSeconds() throws Exception {
+        Thread.sleep(400);
+    }
+
+
+    @Test
+    public void testThatTakesOneSeconds() throws Exception {
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testThatTakesTwoSeconds() throws Exception {
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testThatTakesThreeSeconds() throws Exception {
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void testThatTakesFourSeconds() throws Exception {
+        Thread.sleep(4000);
+    }
+
+
+}
