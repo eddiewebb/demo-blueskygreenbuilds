022babfef59a95f8801c03ae3c8e359ff80dd0b3
Eddie Webb <ollitech@gmail.com>
2020-01-11 13:05:38 -0500

    fix erroes

--- src/test/java/com/edwardawebb/circleci/demo/it/AbstractWebIT.java
+++ src/test/java/com/edwardawebb/circleci/demo/it/AbstractWebIT.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
