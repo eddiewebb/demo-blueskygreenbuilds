f6e08153a9e9d7fa1252e246838d335772adc957
Eddie Webb <ollitech@gmail.com>
2021-11-01 15:54:25 -0400

    update namespace to new cluster, add make SA script

---
+++ vamp/make-service-account.sh
@@ -0,0 +1,10 @@
+svcuser=${1}
+
+kubectl create serviceaccount ${svcuser}
+
+kubectl create clusterrolebinding ${svcuser} --clusterrole=cluster-admin --serviceaccount=default:${svcuser}
+
+
+kubectl get secret $(kubectl get serviceaccounts ${svcuser} -o go-template='{{(index .secrets 0).name}}') -o yaml
+
+echo "Add the token value above to Env Vars, and add ca.crt to envvars or config (public on API)"
