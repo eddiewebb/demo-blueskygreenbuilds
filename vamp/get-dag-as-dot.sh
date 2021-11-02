6062495b74b42cb499e49a7838acfd4eca57dcf5
Eddie Webb <ollitech@gmail.com>
2021-11-02 10:01:09 -0400

    capture contour dag

--- vamp/get-dag-as-dot.sh
+++ vamp/get-dag-as-dot.sh
@@ -3,5 +3,6 @@ CONTOUR_POD=$(kubectl -n projectcontour get pod -l app=contour -o name | head -1
 # Do the port forward to that pod
 kubectl -n projectcontour port-forward $CONTOUR_POD 6060 &
 sleep 2
-curl localhost:6060/debug/dag | dot -T png > contour-dag.png
+curl localhost:6060/debug/dag > contour-dag.dot
+dot contour-dag.dot -T png > contour-dag.png
 kill %1
