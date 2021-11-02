8f95f4d43ad9a2c2e4cb03479fa9dcb53587577b
Eddie Webb <ollitech@gmail.com>
2021-11-02 09:33:08 -0400

    push new release

---
+++ vamp/get-dag-as-dot.sh
@@ -0,0 +1,7 @@
+# Port forward into the contour pod
+CONTOUR_POD=$(kubectl -n projectcontour get pod -l app=contour -o name | head -1)
+# Do the port forward to that pod
+kubectl -n projectcontour port-forward $CONTOUR_POD 6060 &
+sleep 2
+curl localhost:6060/debug/dag | dot -T png > contour-dag.png
+kill %1
