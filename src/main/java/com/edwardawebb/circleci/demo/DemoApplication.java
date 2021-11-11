5a79d827014799560b37429da45781910632c319
Eddie Webb <ollitech@gmail.com>
2021-11-11 16:56:27 -0500

    add docs to spring app and containter too

--- src/main/java/com/edwardawebb/circleci/demo/DemoApplication.java
+++ src/main/java/com/edwardawebb/circleci/demo/DemoApplication.java
@@ -1,12 +1,25 @@
 package com.edwardawebb.circleci.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends WebMvcConfigurerAdapter {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations(
+                        "file:///docs/")
+                .setCachePeriod(0);
+}
 }
