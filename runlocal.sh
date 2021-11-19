4b7c28d4606930bb52c53c316b20f0b192d2d14a
Eddie Webb <ollitech@gmail.com>
2021-11-19 09:38:05 -0500

    update run local

--- runlocal.sh
+++ runlocal.sh
@@ -6,4 +6,8 @@
               -Dbuild.user=local \
               -Dbuild.repo=local
 
-docker run -p8080:8080 eddiewebb/blueskygreenbuilds-demo:blocal
+#refresh image
+docker pull eddiewebb/blueskygreenbuilds-demo:blocal
+
+# to run the container locally we mimic envvars poassed by k8s
+docker run -p8080:8080 -e MY_POD_NAME=local -e MY_POD_IP="1234" eddiewebb/blueskygreenbuilds-demo:blocal
